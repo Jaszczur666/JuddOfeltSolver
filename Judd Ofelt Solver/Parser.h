d4162be86dc62e26436ef9012d852579ad1ffe8f
Jarosław Komar <jaszczur666@o2.pl>
2013-09-13 07:40:58 +0200

    beginnings of load function

--- Judd Ofelt Solver/Parser.h
+++ Judd Ofelt Solver/Parser.h
@@ -1,3 +1,23 @@
-void LoadAbsoDataFromFile(String filename,Experiment &experimental);
+void MarshalString ( String ^ s, string& os ) {
+	using namespace Runtime::InteropServices;
+	const char* chars = 
+		(const char*)(Marshal::StringToHGlobalAnsi(s)).ToPointer();
+	os = chars;
+	Marshal::FreeHGlobal(IntPtr((void*)chars));
+}
+
+void MarshalString ( String ^ s, wstring& os ) {
+	using namespace Runtime::InteropServices;
+	const wchar_t* chars = 
+		(const wchar_t*)(Marshal::StringToHGlobalUni(s)).ToPointer();
+	os = chars;
+	Marshal::FreeHGlobal(IntPtr((void*)chars));
+}
+void LoadAbsoDataFromFile(String^ Filename,Experiment &experimental);
 {
+	double n, j;
+	MarshalString(Filename,name);
+	ifstream inpfile(name);
+	getline(inpfile,str);
+	n<<str;
 }
